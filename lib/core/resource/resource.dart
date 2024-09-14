c186a0e5a658add9deb29bac409bfd025e7b810f
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-09-14 16:13:38 +0300

    add project assets

---
+++ lib/core/resource/resource.dart
@@ -0,0 +1,28 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class R {
+  const R._();
+
+  static const String ASSETS_AIR_PNG = 'assets/air.png';
+
+  static const String ASSETS_AIR2_PNG = 'assets/air2.png';
+
+  static const String ASSETS_BOOK_SVG = 'assets/book.svg';
+
+  static const String ASSETS_DOT_SVG = 'assets/dot.svg';
+
+  static const String ASSETS_FLIGHT_SVG = 'assets/flight.svg';
+
+  static const String ASSETS_FLY_SVG = 'assets/fly.svg';
+
+  static const String ASSETS_FROM_TO_PNG = 'assets/from_to.png';
+
+  static const String ASSETS_FROM_TO_HOME_PNG = 'assets/from_to_home.png';
+
+  static const String ASSETS_ICON_SVG = 'assets/icon.svg';
+
+  static const String ASSETS_MAP_SVG = 'assets/map.svg';
+
+  static const String ASSETS_SEARCH_SVG = 'assets/search.svg';
+}
